66d66b7770772e109a43cfd1aeea01ed8b8e4ab9
codebillions <niyijames292@gmail.com>
2023-03-12 21:42:48 +0100

    Updated the interface
    ui update

--- js/gridtek.js
+++ js/gridtek.js
@@ -1,1 +1,25 @@
 AOS.init();
+
+// form validation
+
+function validate() {
+    'use strict'
+
+    // Fetch all the forms assoc Bootstrap validation
+    var forms = document.querySelectorAll( '.needs-validation' )
+
+    // Loop over them and prevent submission
+    Array.prototype.slice.call( forms )
+        .forEach( function ( form ) {
+            form.addEventListener( 'submit', function ( event ) {
+                if ( !form.checkValidity() ) {
+                    event.preventDefault()
+                    event.stopPropagation()
+                }
+
+                form.classList.add( 'was-validated' )
+            }, false )
+        } )
+};
+
+validate()
